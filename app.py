d5792d0b14fb7e439326a9654f7b3df873b7c790
Donna Nur Tamara <161492059+donnatamara@users.noreply.github.com>
2025-05-12 06:14:17 +0700

    next oop

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+# import library
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 
+# menampilkan header website
 st.write("""
 # Prediksi Spesies Penguin Palmer
 
@@ -14,48 +16,55 @@ Aplikasi yang akan membantu kamu memprediksi spesies penguin berdasarkan ciri-ci
 Data yang digunakan berasal dari kaggle [Palmer Penguins](https://www.kaggle.com/datasets/ashkhagan/palmer-penguins-datasetalternative-iris-dataset/data) yang dikembangkan oleh Dr. Kristen Gorman
 """)
 
+# sidebar untuk input variabel
 st.sidebar.header('Fitur Input Variabel ')
 
+# instruksi untuk input dari pengguna
 st.sidebar.markdown("""
 Silakan masukkan detail karakteristik penguin di bawah ini untuk memprediksi spesiesnya.
 """)
 
-# Collects user input features into dataframe
+# mengumpulkan fitur input yang diberikan oleh pengguna ke dalam dataframe
 uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
-    def user_input_features():
+    def user_input_features(): #input pengguna untuk fitur-fitur penguin
         island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
         sex = st.sidebar.selectbox('Sex',('male','female'))
         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
         flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
         body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
+
+        # membuat dataframe dari input pengguna
+        data = {
+                'island': island,
                 'bill_length_mm': bill_length_mm,
                 'bill_depth_mm': bill_depth_mm,
                 'flipper_length_mm': flipper_length_mm,
                 'body_mass_g': body_mass_g,
-                'sex': sex}
+                'sex': sex
+        }
         features = pd.DataFrame(data, index=[0])
         return features
-    input_df = user_input_features()
+    input_df = user_input_features() # mengambil input dari pengguna
 
-# Gabungkan input user dengan data asli untuk encoding
+# membaca dataset penguin asli dan menghapus kolom spscies untuk encoding
 penguins_raw = pd.read_csv('C:/Users/User/PASD/Dataset/penguins_cleaned.csv')
 penguins = penguins_raw.drop(columns=['species'])
-df = pd.concat([input_df,penguins],axis=0)
+df = pd.concat([input_df, penguins],axis=0) # menggabungkan input user dengan data asli untuk encoding
 
-# Encoding kolom kategorikal
+# encoding kolom kategorikal
 encode = ['sex','island']
 for col in encode:
     dummy = pd.get_dummies(df[col], prefix=col)
     df = pd.concat([df,dummy], axis=1)
     del df[col]
-df = df[:1] # Ambil hanya data input user
 
-# Tampilkan input pengguna
+df = df[:1] # ambil hanya data input user
+
+# menampilkan input pengguna
 st.subheader('Fitur Input Pengguna')
 
 if uploaded_file is not None:
@@ -63,20 +72,20 @@ if uploaded_file is not None:
 else:
     st.write(df)
 
-# Reads in saved classification model
+# membaca model yang telah disimpan (classifier yang sudah dilatih)
 load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
 
-# Apply model to make predictions
+# menggunakan model untuk memprediksi spesies penguin berdasarkan input
 prediction = load_clf.predict(df)
 prediction_proba = load_clf.predict_proba(df)
 
-# Hasil Prediksi
+# hasil prediksi spesies 
 st.subheader('Prediksi')
 penguins_species = np.array(['Adelie', 'Gentoo', 'Chinstrap'])
 hasil_prediksi = penguins_species[prediction][0]
 st.write(f"Spesies yang diprediksi: **{hasil_prediksi}**")
 
-# Tampilkan gambar berdasarkan spesies yang diprediksi
+# menampilkan gambar berdasarkan spesies yang diprediksi
 if hasil_prediksi == 'Adelie':
     st.image(
         "https://upload.wikimedia.org/wikipedia/commons/e/e3/Hope_Bay-2016-Trinity_Peninsula%E2%80%93Ad%C3%A9lie_penguin_%28Pygoscelis_adeliae%29_04.jpg",
@@ -98,6 +107,6 @@ elif hasil_prediksi == 'Chinstrap':
         use_container_width=True  
     )
 
-# Peluang prediksi
+# menampilkan probabilitas prediksi
 st.subheader('Probabilitas Prediksi')
 st.write(prediction_proba)
--- build_model.py
+++ build_model.py
@@ -1,31 +1,35 @@
+# import Library
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+import pickle
 
+# membaca dataset yang sudah dibersihkan
 penguins = pd.read_csv("C:/Users/User/PASD/Dataset/penguins_cleaned.csv")
 
-df = penguins.copy()
-target = 'species'
+df = penguins.copy() # duplikasi data untuk diproses
+target = 'species' # menentukan kolom target dan kolom kategorikal yang akan diencoding
 encode = ['sex','island']
 
+# melakukan one-hot encoding pada kolom kategorikal
 for col in encode:
     dummy = pd.get_dummies(df[col], prefix=col)
     df = pd.concat([df,dummy], axis=1)
     del df[col]
 
-target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
-def target_encode(val):
+target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2} # mapping label target ke angka
+def target_encode(val): # encode nilai target
     return target_mapper[val]
 
+# mengubah nilai target menjadi numerik
 df['species'] = df['species'].apply(target_encode)
 
-# Separating X and y
+# memisahkan fitur dan label (x dan y)
 fitur = df.drop('species', axis=1)
 label = df['species']
 
-# Build random forest model
-clf = RandomForestClassifier()
-clf.fit(fitur, label)
+# membuat dan melatih random forest
+random_forest_model = RandomForestClassifier()
+random_forest_model.fit(fitur, label)
 
-# Saving the model
-import pickle
-pickle.dump(clf, open('penguins_clf.pkl', 'wb'))
+# menyimpan model ke file pickle
+pickle.dump(random_forest_model, open('penguins_clf.pkl', 'wb'))
