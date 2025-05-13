cdb88727be4006b66c0c783d982b186ae3f08290
Donna Nur Tamara <161492059+donnatamara@users.noreply.github.com>
2025-05-13 10:13:11 +0700

    changes opp

---
+++ input_handler.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+
+class InputHandler:
+    def __init__(self):
+        self.uploaded_file = None
+        self.input_df = None
+
+    def user_input_features(self):
+        island = st.sidebar.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+        sex = st.sidebar.selectbox('Sex', ('male', 'female'))
+        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+
+        data = {
+            'island': island,
+            'bill_length_mm': bill_length_mm,
+            'bill_depth_mm': bill_depth_mm,
+            'flipper_length_mm': flipper_length_mm,
+            'body_mass_g': body_mass_g,
+            'sex': sex
+        }
+        return pd.DataFrame(data, index=[0])
+
+    def load_input(self):
+        self.uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+        if self.uploaded_file is not None:
+            self.input_df = pd.read_csv(self.uploaded_file)
+        else:
+            self.input_df = self.user_input_features()
+        return self.input_df
