afd1232a496481dc7d41f7d1e6346706b5307aca
Donna Nur Tamara <161492059+donnatamara@users.noreply.github.com>
2025-05-27 01:14:44 +0700

    add comment

--- build_model.py
+++ build_model.py
@@ -1,6 +1,12 @@
-from model_loader import ModelLoader
+from model_loader import ModelLoader # Mengimpor class ModelLoader dari file model_loader.py
 
+# Mengeksekusi kode hanya jika file ini dijalankan secara langsung
 if __name__ == '__main__':
+    # Membuat objek dari class ModelLoader
     loader = ModelLoader()
+
+    # Membangun model dari data dan menyimpannya ke file
     model = loader.build_model()
+
+    # Menampilkan pesan bahwa model berhasil dibuat dan disimpan
     print("Model berhasil dibangun dan disimpan.")
